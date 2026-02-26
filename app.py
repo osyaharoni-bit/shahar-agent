43c14c511e32c3e3f5bdb81f3d9b5f449776ff1a
osyaharoni-bit <osyaharoni@gmail.com>
2026-02-26 16:20:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -373,8 +373,9 @@ if __name__ == "__main__":
     print("  🌐  http://127.0.0.1:5000")
     print("━" * 52)
 
-   import os
+import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
